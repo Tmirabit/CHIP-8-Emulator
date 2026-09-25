b445dab3f376279ae3d00aa48c35dc52b8238a19
Tmirabit <tylermirabitur55@gmail.com>
2026-09-24 22:21:54 -0400

    Added majority of opcodes

--- src/chip8.c
+++ src/chip8.c
@@ -41,6 +41,11 @@ void chip8_Cycle(Chip8 *chip8)
 	// Fetch Opcode
 	unsigned short opcode = (chip8->memory[chip8->pc] << 8) | 
 							 chip8->memory[chip8->pc+1];
+	unsigned char X = (opcode & 0x0F00) >> 8;
+	unsigned char Y = (opcode & 0x00F0) >> 4;
+	unsigned char N = opcode & 0x000F;
+	unsigned char NN = opcode & 0x00FF;
+	unsigned short NNN = opcode & 0x0FFF;
 
 	// Decode Opcode
 	switch(opcode & 0xF000)
@@ -49,71 +54,135 @@ void chip8_Cycle(Chip8 *chip8)
 			switch(opcode & 0x000F)
 			{
 				case 0x0000: // 0x00E0
-				break;
+					break;
 
 				case 0x000E: // 0x00EE
-				break;
+					return;
 
 				default:
 					printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
 			}
 		case 0x1000: // 0x1NNN
+			chip8->pc = NNN;
 			break;
 		case 0x2000: // 0x2NNN
+			chip8->stack[chip8->sp] = chip8->pc;
+			++chip8->sp;
+			chip8->pc = NNN;
 			break;
 		case 0x3000: // 0x3XNN
-			break;
+			if (chip8->V[X] == NN)
+				chip8->pc += 4;
+			else
+				chip8->pc += 2;
+			break; 
 		case 0x4000: // 0x4XNN
+			if (chip8->V[X] != NN);
+				chip8->pc += 4;
+			else
+				chip8->pc += 2;
 			break;
 		case 0x5000: // 0x5XY0
+			if (chip8->V[X] == chip8->V[Y])
+				chip8->pc += 4;
+			else
+				chip8->pc += 2;
 			break;
 		case 0x6000: // 0x6XNN
+			chip8->V[X] = NN;
+			chip8->pc += 2;
 			break;
 		case 0x7000: // 0x7XNN
+			chip8->V[X] += NN;
+			chip8->pc += 2;
 			break;
 		case 0x8000:
 			switch(opcode & 0x000F)
 			{
 				case 0x0000: // 0x8XY0
+					chip8->V[X] = chip8->V[Y];
+					chip8->pc += 2;
 					break;
 				case 0x0001: // 0x8XY1
+					chip8->V[X] |= chip8V[Y];
+					chip8->pc += 2;
 					break;
 				case 0x0002: // 0x8XY2
+					chip8->V[X] &= chip8->V[Y];
+					chip8->pc += 2;
 					break;
 				case 0x0003: // 0x8XY3
+					chip8->V[X] ^= chip8->V[Y];
+					chip8->pc += 2;
 					break;
 				case 0x0004: // 0x8XY4
+					if(chip8->V[Y] > (0xFF - V[X])
+						chip8->V[0xF] = 1; // Carry
+					else
+						chip8->V[0xF] = 0;
+					chip8->V[X] += chip8->V[Y];
+					chip8->pc += 2;
 					break;
 				case 0x0005: // 0x8XY5
+					if(chip8->V[X] >= chip8->V[Y])
+						chip8->V[0xF] = 1;
+					else
+						chip8->V[0xF] = 0;
+					chip8->V[X] -= chip8->V[Y];
+					chip8->pc += 2;
 					break;
 				case 0x0006: // 0x8XY6
+					chip8->V[0xF] = chip8->V[X] & 0x01;
+					chip8->V[X] >>= 1;
+					chip8->pc += 2;
 					break;
 				case 0x0007: // 0x8XY7
+					if(chip8->V[Y] >= chip8->V[X])
+						chip8->V[0xF] = 1;
+					else
+						chip8->V[0xF] = 0;
+					chip8->V[X] = chip8->V[Y] - chip8->V[X];
+					chip8->pc += 2;
 					break;
 				case 0x000E: // 0x8XYE
+					if(chip8->V[X] & 0x10)
+						chip8->V[0xF] = 1;
+					else
+						chip8->V[0xF] = 0;
+					chip8->V[X] <<= 1;
+					chip8->pc += 2;
 					break;
 				default:
 					printf("Unknown opcode [0x8000]:  0x%X\n", opcode);
 			}
 		case 0x9000: // 0x9XY0
+			if(chip8->V[X] != chip8->V[Y])
+				chip8->pc += 4;
+			else
+				chip8->pc += 2;
 			break;
 		case 0xA000: // 0xANNN
-			chip8->I = opcode & 0x0FFF;
+			chip8->I = NNN;
 			chip8->pc += 2;
 			break;
 		case 0xB000: // 0xBNNN
-			chip8->pc = chip8->V[0] + (opcode & 0x0FFF);
+			chip8->pc = chip8->V[0] + NNN;
 			break;
 		case 0xC000: // 0xCXNN
+			chip8->V[X] = (rand()%256) & NN;
+			chip8->pc += 2;
 			break;
 		case 0xD000: // 0xDXYN
+			// Display Code to fix
 			break;
 		case 0xE000:
 			switch(opcode & 0x000F)
 			{
 				case 0x000E: // 0xEX9E
+					// Key Press
 					break;
 				case 0x0001: // 0xEXA1
+					// Key Press
 					break;
 				default:
 					printf("Unknown opcode [0xE000]:  0x%X\n", opcode);
@@ -122,24 +191,48 @@ void chip8_Cycle(Chip8 *chip8)
 			switch(opcode & 0x000F)
 			{
 				case 0x0007: // 0xFX07
+					chip8->V[X] = chip8->delay_timer;
+					chip8->pc += 2;
 					break;
 				case 0x000A: // 0xFX0A
+					// Key Press Code
 					break;
 				case 0x0008: // 0xFX18
+					chip8->sound_timer = chip8->V[X];
+					chip8->pc += 2;
 					break;
 				case 0x000E: // 0xFX1E
+					chip8->I += chip8->V[X];
+					chip8->pc += 2;
 					break;
 				case 0x0009: // 0xFX29
+					// Sprite font thing 
 					break;
 				case 0x0003: // 0xFX33
+					chip8->memory[chip8->I] = chip8->V[X] / 100;
+					chip8->memory[chip8->I + 1] = (chip8->V[X] / 10) % 10;
+					chip8->memory[chip8->I + 2] = (chip8->V[X] % 100) % 10;
+					chip8->pc += 2;
 					break;
 				case 0x0005: 
 					if((opcode & 0x00F0) == 0x0050){	
 						// 0xFX55
+						for(int i=0; i <= X; i++)
+						{	
+							chip8->memory[chip8->I + i] = chip8=>V[i];
+						}
+						chip8->pc += 2;
 					}else if((opcode &0x00F0) == 0x0010){	
 						// 0xFX15
+						chip8->delay_timer = chip8->V[X];
+						chip8->pc += 2;
 					}else{
 						// 0xFX65
+						for(int i=0; i <= X; i++)
+						{
+							chip8->V[i] = chip8->memory[chip8->I + i];
+						}
+						chip8->pc += 2;
 					}
 					break;
 				default:					
@@ -148,6 +241,7 @@ void chip8_Cycle(Chip8 *chip8)
 		default:
 			printf("Unknown opcode: 0x%X\n", opcode);
 	}
+
 	//Update timers
 	if(chip8->delay_timer > 0)
 	{
